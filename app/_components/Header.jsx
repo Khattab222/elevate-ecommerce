0741a77e55207b28d05d70b6ca28af0dbe862d6d
khattabk222 <https://github.com/Khattab222>
2024-10-30 21:17:46 +0300

    start header

---
+++ app/_components/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+  return (
+    <div className="   text-center uppercase tracking-widest text-5xl font-extrabold py-2 bg-gradient-to-r  from-orange-700 to-yellow-400 bg-clip-text text-transparent w-fit mx-auto">elevate</div>
+  )
+}
+
+export default Header
